a7e71c075aa7dc373da8929b0dc93ca8ee87119d
gita <tomas.arias@udea.edu.co>
2017-12-01 16:35:41 -0500

    corrección de datos de validación en index.

--- forms.py
+++ forms.py
@@ -6,6 +6,18 @@ import pandas as pd
 import numpy as np
 import os
 import json
+import pprint
+
+
+##Crear cliente
+#client = MongoClient()
+#
+##Crear database
+#db = client.test_database
+#
+##Crear colección
+#collection = db.test_collection
+#collection.delete_many({})
 
 # App config.
 DEBUG = True
@@ -13,6 +25,20 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
+
+#Crear Base de datos
+#Crear cliente
+client = MongoClient()
+
+#Crear database
+db = client.IPS_database
+
+
+#Delete collection
+#db.Index_collection.delete_many({})
+
+#Crear colección
+IPS_data  = db.Index_collection
 
 class ReusableForm(Form):
     name = TextField('Nombre:', validators=[validators.required()])
@@ -25,6 +51,7 @@ class ReusableForm(Form):
 
 @app.route("/", methods=['GET', 'POST'])
 def datos_ips():
+    
 #    form = ReusableForm(request.form)
 
     #Directorio de proyecto
@@ -41,18 +68,47 @@ def datos_ips():
         cities[idx] = list(np.unique(df[df['Departamento']==idx]['Municipio']))
 
     if request.method == 'POST':
-        dpto = request.form['dpto']
-        city = request.form['city']
+        #Datos IPS
         name = request.form['name']
+        nit = request.form['nit']
+        car = request.form['car']
+        ger = request.form['ger']
+        niv_opt = request.form['nivel']
+        hab_opt = request.form['habil']
+        dpto = request.form['dpto']
+        city = request.form['city']        
         addr = request.form['addr']
         tel = request.form['tel']
         email = request.form['email']
-        niv_opt = request.form['nivel']
-        hab_opt = request.form['habil']
-        car = request.form['car']
-
+        
+        #Datos de quien llena la encuesta
+        usrname = request.form['username']
+        usrid = request.form['userid']
+        usrjob = request.form['userjob']
+        
+        index_data = {"Nombre IPS":name,
+                      "NIT":nit,
+                      "Caracter":car,
+                      "Nombre del gerente":ger,
+                      "Nivel de IPS":niv_opt,
+                      "Habilitada":hab_opt,                      
+                      "Departamento":dpto,
+                      "Ciudad":city,
+                      "Dirección":addr,
+                      "Telefono":tel,
+                      "e-mail":email,
+                      "Nombre del responsable":usrname,
+                      "ID del responsable":usrid,
+                      "Cargo del responsable":usrjob}
+        
+        #Insertar nueva IPS
+        IPS_data.insert_one(index_data).inserted_id
+        
         print (name, " ", dpto, " ", city, " ",car , " ",niv_opt, " ", addr, " ", tel, " ", email)
-
+        print(db.collection_names(include_system_collections=False))
+        for docs in IPS_data.find():
+            pprint.pprint(docs)
+            print('--------------------------------')
 #        if form.validate():
 #            # Save the comment here.
 #            flash('Gracias por registrarse ' + name)
