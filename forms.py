a043395fa56df4e59ddbe49a74e679e604842aba
jcvasquezc <juancamilo0628@gmail.com>
2018-04-05 18:06:01 -0500

    ok

--- forms.py
+++ forms.py
@@ -644,11 +644,14 @@ def validar(modulo):
         for j in request.form:
             if int(modulo)==1 and len(request.form[j])>0:
                 IPS_data.find_and_modify(query={"ID":usr['ID']}, update={"$set": {"valmod1": True}}, upsert=False, full_response= True)
-                dict_encuesta[j]=request.form[j]
+                dict_encuesta[j]=request.form.getlist(j)
+                if len(dict_encuesta[j])==1:
+                    dict_encuesta[j]=dict_encuesta[j][0]
                 continue
             if j.find("question")>=0:
-                dict_encuesta[j]=request.form[j]
-
+                dict_encuesta[j]=request.form.getlist(j)
+                if len(dict_encuesta[j])==1:
+                    dict_encuesta[j]=dict_encuesta[j][0]
         IPS_data.find_and_modify(query={"ID":usr['ID']}, update={"$set": {"Resultados Modulo "+str(modulo): dict_encuesta}}, upsert=False, full_response= True)
        
         return render_template('validar.html', nit=usr['Codigo'])
